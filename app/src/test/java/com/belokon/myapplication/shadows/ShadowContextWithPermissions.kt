88af2abdd6b2f33d79f78e320a533317667b2057
Alexander <belokon202@gmail.com>
2019-01-23 23:14:13 +0300

    added permission dispatcher tests

--- app/src/test/java/com/belokon/myapplication/shadows/ShadowContextWithPermissions.kt
+++ app/src/test/java/com/belokon/myapplication/shadows/ShadowContextWithPermissions.kt
@@ -1,13 +1,19 @@
 package com.belokon.myapplication.shadows
 
 import android.Manifest
+import android.content.pm.PackageManager
 import org.robolectric.annotation.Implements
 import org.robolectric.shadows.ShadowContextImpl
 
 @Implements(className = ShadowContextImpl.CLASS_NAME)
 class ShadowContextWithPermissions : ShadowContextImpl() {
-    private val allowedPermissions = listOf(Manifest.permission.ACCESS_FINE_LOCATION)
+
+    companion object {
+        const val ALLOWED_PERMISSION = Manifest.permission.ACCESS_FINE_LOCATION
+    }
+
+    private val allowedPermissions = listOf(ALLOWED_PERMISSION)
     override fun checkPermission(permission: String?, pid: Int, uid: Int): Int {
-        return if (permission in allowedPermissions) 0 else super.checkPermission(permission, pid, uid)
+        return if (permission in allowedPermissions) PackageManager.PERMISSION_GRANTED else super.checkPermission(permission, pid, uid)
     }
 }
